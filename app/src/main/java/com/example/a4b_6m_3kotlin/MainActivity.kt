3afd50ed72a3fff0c127fc27ea1d67da012be9f8
Otabek <boboyevotabek551@gmail.com>
2022-05-20 23:18:43 +0500

    Initial commit

---
+++ app/src/main/java/com/example/a4b_6m_3kotlin/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.a4b_6m_3kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a4b_6m_3kotlin.adapter.AdapterMember
+import com.example.a4b_6m_3kotlin.model.Member
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    lateinit var recyclerview: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView ()
+    }
+
+    fun initView() {
+        recyclerview = findViewById(R.id.recyclerview)
+        recyclerview.layoutManager = GridLayoutManager(this, 1)
+        recyclerview.isNestedScrollingEnabled=false
+
+
+        var member: List<Member> = MemberAdd()
+        refreshAdapter(member)
+    }
+
+    fun refreshAdapter(member: List<Member>) {
+
+        var adapter = AdapterMember(member)
+        recyclerview.adapter = adapter
+    }
+
+    fun MemberAdd(): List<Member> {
+
+        var member = ArrayList<Member>()
+
+        for (i in 0..20 step 1) {
+            member.add(Member("Otabek " + i, "Boboyev " + i))
+        }
+        return member
+    }
+}
